36a56cfebb82997d5d62d968ced1e1c057c352b4
Eric Pugh <pugh.eric@gmail.com>
2019-01-07 00:30:47 -0500

    Removed boilerplate, added AutoComplete and Artwork components

---
+++ src/components/Artwork.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="artwork">
+        <h1>insert artwork here!</h1>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped lang="scss">
+    .artwork {
+        background-color: #EFEFEF;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <img alt="Vue logo" src="../assets/logo.png">
+    </header>
+    <AutoComplete :items="[ '1994.17', '1984.149.1', '1968.155.8', '1968.155.64']" />
+    <Artwork/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import AutoComplete from "@/components/AutoComplete.vue";
+import Artwork from "@/components/Artwork.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    AutoComplete,
+    Artwork
   }
 };
 </script>
